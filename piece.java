6265272b680d8447dbfaff9b8126fbdf3644a524
Lukasgrat <60186475+Lukasgrat@users.noreply.github.com>
2021-01-07 14:06:47 -0500

    Create piece.java

---
+++ piece.java
@@ -0,0 +1,24 @@
+import javafx.scene.text.Text;
+import javafx.scene.paint.Color;
+public class piece{ 
+    public int location;
+    public boolean isWhite;
+    public Text text;
+    public int xLocation;
+    public int yLocation;
+    public  piece(int location, boolean isWhite, String text){
+        this.location = location;
+        this.text = new Text(text);
+        this.xLocation = this.location%12*50+10;
+        this.yLocation = this.location/12*50+25;
+        this.text.setX(this.xLocation);
+        this.text.setY(this.yLocation);
+        this.isWhite = isWhite;
+        if(this.isWhite == true){
+            this.text.setStroke(Color.WHITE);
+        }
+        else{
+            this.text.setStroke(Color.BLACK);
+        }
+    }
+}
